89521572d0d80840c48083a46bb9093dd987d22f
fanzang2 <fanzang2@fa23-cs527-086.cs.illinois.edu>
2023-10-08 18:50:48 -0500

    Add Halstead metric sample result and complete cyclomatic caclulation for inherit functions in each project

--- complexity_metric.py
+++ complexity_metric.py
@@ -3,19 +3,25 @@ import re
 import os
 import fnmatch
 import csv
+import json
 
 # using checkstyle tool to walk through test file to calculate cyclomatic complexity of each test method
 
 exe_command = 'java -jar checkstyle-10.12.3-all.jar -c'
 cyclo_config_file = '/home/fanzang2/project/complexity-based-tcp/cyclomatic_check.xml'
-len_config_file = '/home/fanzang2/project/complexity-based-tcp/method_len.xml'
 #test_directory = '/home/fanzang2/project/app/ctest-hadoop/hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/'
-#test_directory = '/home/fanzang2/project/app/ctest-hadoop/hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/'
+test_directory = '/home/fanzang2/project/app/ctest-hadoop/hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop'
 #test_directory = '/home/fanzang2/project/app/ctest-alluxio/core/'
 #test_directory = '/home/fanzang2/project/app/ctest-hbase/hbase-server/src/test/java/org/apache/hadoop/hbase/'
-test_directory = '/home/fanzang2/project/app/ctest-zookeeper/zookeeper-server/src/test/java/org/apache/zookeeper/'
-package_name_start = 'org'
-tsv_file_path = 'zookeeper-server.tsv'
+#test_directory = '/home/fanzang2/project/app/ctest-zookeeper/zookeeper-server/src/test/java/org/apache/zookeeper/'
+project_name = 'hadoop-hdfs'
+complexity_info = {}
+complexity_tsv_file_path = project_name + '.tsv'
+test_class_info = {}
+test_class_info_json_path = project_name + '.json'
+load_complexity_info = None
+load_class_info = None
+
 
 def extract_java_files(directory: str):
     # extract all java test file from given directory
@@ -87,39 +93,111 @@ def extract_package_name(java_file_path):
             return '' 
 
 
+def extract_class_name(java_file_path, package_name):
+    class_name = java_file_path.split('/')[-1][:-5]
+    parent_class_with_package = None
+
+    with open(java_file_path, 'r') as file:
+        java_code = file.read()
+        class_pattern = re.compile(r'class\s+'+ class_name + '\s+extends\s+([\w.]+)')
+        class_match = class_pattern.search(java_code)
+
+        if class_match:
+            parent_class = class_match.group().split(' ')[-1]
+        else:
+            return class_name, parent_class_with_package
+        import_pattern = re.compile(r'import\s+([\w.]+)' + parent_class + ';')
+        import_match = import_pattern.search(java_code)
+        if import_match:
+            parent_class_with_package = import_match.group().split(' ')[1][:-1]
+        else:
+            parent_class_with_package = package_name + '.' + parent_class
+    return class_name, parent_class_with_package
+
+
+def process_checkstyle_output(checkstyle_output):
+    package_name = checkstyle_output[0].split(':')[-1].split("'")[1]
+    method_complexity_list = []
+    method_name_list = []
+    i = 0
+    while i + 1 < len(checkstyle_output):
+        if checkstyle_output[i].endswith('[CyclomaticComplexity]') and checkstyle_output[i + 1].endswith('[MethodLength]'):
+            curr = checkstyle_output[i].split(':')
+            method_complexity_list.append(int(curr[-1].split(' ')[4]))
+            curr = checkstyle_output[i + 1].split(':')
+            method_name_list.append(curr[-1].split(' ')[2])
+            i += 2
+        else:
+            i += 1
+    return package_name, method_name_list, method_complexity_list
+
+
 
 def extract_method_complexity(java_file_path):
     command = exe_command + ' ' + cyclo_config_file + ' ' + java_file_path
     result = subprocess.run(command, shell=True, cwd='./', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     output = result.stdout.splitlines()
-    # method_start_lines = []
-    complexity_list = []
-    for i in range(1, len(output) - 1):
-        curr = output[i].split(':')
-        #method_start_lines.append(int(curr[1]))
-        complexity_list.append(int(curr[-1].split(' ')[4]))
-    #method_names = extract_method_names(java_file_path, method_start_lines)
-    method_names = extract_method_name(java_file_path)
-    package_name = extract_package_name(java_file_path)
-    if package_name == '':
-        package_name = java_file_path[java_file_path.find(package_name_start):].replace('/', '.')[:-5]
-    for i in range(len(method_names)):
-        if method_names[i] == 'NuLL':
-            continue
-        method_names[i] = package_name + '#' + method_names[i]
-    result_dict = {}
-    for i in range(len(method_names)):
-        if method_names[i] == 'NuLL':
-            continue
-        result_dict[method_names[i]] = complexity_list[i]
-    return result_dict
+    checkstyle_output = output[1:-1]
+    package_name, method_name_list, method_complexity_list = process_checkstyle_output(checkstyle_output)
+    class_name, parent_class_with_package = extract_class_name(java_file_path, package_name)
+    test_class_name = package_name + '.' + class_name
+    test_class_info[test_class_name] = {"parent":parent_class_with_package, "methods":method_name_list}
+    for i in range(len(method_name_list)):
+        complexity_info[test_class_name +'#'+ method_name_list[i]] = method_complexity_list[i]
 
+
+def extract_inherit_method_complexity():
+    for name, info in test_class_info.items():
+        parent_class = info["parent"]
+        while parent_class != None:
+            if parent_class not in test_class_info:
+                break
+            parent_methods = test_class_info[parent_class]["methods"]
+            for m in parent_methods:
+                complete_name = parent_class + '#' + m
+                if m not in info["methods"]:
+                    if complete_name in complexity_info:
+                        complexity_info[name +'#'+ m] = complexity_info[complete_name]
+#                    elif load_complexity_info != None and complete_name in load_complexity_info:
+#                        complexity_info[complete_name] = load_complexity_info[complete_name]
+#                        complexity_info[name +'#'+ m] = complexity_info[complete_name]
+            parent_class = test_class_info[parent_class]["parent"]
+
+
+
+def load_calculated_info(complexity_path, info_path):
+    with open(info_path, 'r') as file:
+        load_class_info = json.load(file)
+#    missing_class = []
+#    for name, info in test_class_info.items():
+#        parent_class = info["parent"]
+#        if parent_class not in test_class_info and parent_class in load_class_info:
+#            missing_class.append(parent_class)
+#    for m in missing_class:
+#        test_class_info[m] = load_class_info[m]
+    test_class_info.update(load_class_info)
+    load_complexity_info = {}
+    with open(complexity_path, 'r') as tsv_file:
+        for line in tsv_file:
+            method, metric = line.strip().split('\t')
+            load_complexity_info[method] = metric
+    complexity_info.update(load_complexity_info)
 
 java_files = extract_java_files(test_directory)
-with open(tsv_file_path, 'a', newline='') as tsvfile:
+for i in range(len(java_files)):
+    extract_method_complexity(java_files[i])
+    print(f"Complete java file:{java_files[i]}, index: {i}")
+
+load_calculated_info('/home/fanzang2/project/complexity-based-tcp/hadoop-common.tsv', '/home/fanzang2/project/complexity-based-tcp/hadoop-common.json')
+
+with open(test_class_info_json_path, 'w') as json_file:
+    json.dump(test_class_info, json_file)
+
+#print(len(complexity_info))
+extract_inherit_method_complexity()
+#print(len(complexity_info))
+with open(complexity_tsv_file_path, 'a', newline='') as tsvfile:
     writer = csv.writer(tsvfile, delimiter='\t')
-    for i in range(len(java_files)):
-        result_dict = extract_method_complexity(java_files[i])
-        for key, value in result_dict.items():
-            writer.writerow([key, value])
-        print(f"Complete java file:{java_files[i]}, index: {i}")
+    for key, value in complexity_info.items():
+        writer.writerow([key, value])
+
--- separate.py
+++ separate.py
@@ -1,28 +1,62 @@
-#ctest_tsv = '/home/fanzang2/project/ctest-prio-ae/testInfo/execTime/hadoop-common/hadoop-common-testcase.tsv'
+import csv
+
+project_name = ['hadoop-common', 'hadoop-hdfs', 'alluxio-core', 'hbase-server', 'zookeeper-server']
+ctest_tsv = '/home/fanzang2/project/ctest-prio-ae/testInfo/execTime/hadoop-common/hadoop-common-testcase.tsv'
 #ctest_tsv = '/home/fanzang2/project/ctest-prio-ae/testInfo/execTime/hadoop-hdfs/hadoop-hdfs-testcase.tsv'
 #ctest_tsv = '/home/fanzang2/project/ctest-prio-ae/testInfo/execTime/alluxio-core/alluxio-core-testcase.tsv'
 #ctest_tsv = '/home/fanzang2/project/ctest-prio-ae/testInfo/execTime/hbase-server/hbase-server-testcase.tsv'
-ctest_tsv = '/home/fanzang2/project/ctest-prio-ae/testInfo/execTime/zookeeper-server/zookeeper-server-testcase.tsv'
-all_test_file = '/home/fanzang2/project/complexity-based-tcp/zookeeper-server.tsv'
-output_file = '/home/fanzang2/project/complexity-based-tcp/zookeeper-server-direct.tsv'
-missing_file = '/home/fanzang2/project/complexity-based-tcp/zookeeper-server-inherit.tsv'
-used_method = []
-with open(ctest_tsv, 'r') as tsv_file:
-    for line in tsv_file:
-        used_method.append(line.strip().split('\t')[0])
-used_method = set(used_method)
-record = {}
-for i in used_method:
-    record[i] = 0
-with open(output_file, 'a') as out_file:    
-    with open(all_test_file, 'r') as tsv_file:
+#ctest_tsv = '/home/fanzang2/project/ctest-prio-ae/testInfo/execTime/zookeeper-server/zookeeper-server-testcase.tsv'
+all_test_file = '/home/fanzang2/project/complexity-based-tcp/hadoop-common.tsv'
+output_file = '/home/fanzang2/project/complexity-based-tcp/hadoop-common-direct.tsv'
+missing_file = '/home/fanzang2/project/complexity-based-tcp/hadoop-common-inherit.tsv'
+
+
+
+def extract_ctest_cyclomatic(proj_name):
+    ctest_tsv = f"/home/fanzang2/project/ctest-prio-ae/testInfo/execTime/{proj_name}/{proj_name}-testcase.tsv"
+    all_test_file = f"/home/fanzang2/project/complexity-based-tcp/{proj_name}.tsv"
+    output_file = f"/home/fanzang2/project/complexity-based-tcp/{proj_name}-ctest.tsv"
+    used_method = []
+    labeled_method = {}
+    find = {}
+    with open(ctest_tsv, 'r') as tsv_file:
         for line in tsv_file:
-            fields = line.strip().split('\t')
-            if fields and fields[0] in used_method:
-                out_file.write(line)
-                record[fields[0]] = 1
-             
-with open(missing_file, 'a') as out_file:    
-    for i in record:
-        if record[i] == 0:
-            out_file.write(i+'\n')
+            method = line.strip().split('\t')[0]
+            find[method] = 0
+            if method[-1] == ']':
+                name = method.split('[')[0]
+                if name not in labeled_method:
+                    labeled_method[name] = []
+                labeled_method[name].append(method)
+            else:
+                used_method.append(method)
+    used_method = set(used_method)
+    all_methods = {}
+    with open(output_file, 'a', newline='') as out_file:
+        with open(all_test_file, 'r') as tsv_file:
+            tsv_writer = csv.writer(out_file, delimiter='\t')
+            for line in tsv_file:
+                fields = line.strip().split('\t')
+                if fields:
+                    all_methods[fields[0].split('#')[1]] = fields[1]
+                    if fields[0] in used_method:
+                        #out_file.write(line)
+                        tsv_writer.writerow([fields[0], fields[1]])
+                        find[fields[0]] = 1
+                    elif fields[0] in labeled_method:
+                        for m in labeled_method[fields[0]]:
+                            tsv_writer.writerow([m, fields[1]])
+                            find[m] = 1
+    with open(output_file, 'a', newline='') as out_file:
+        tsv_writer = csv.writer(out_file, delimiter='\t')
+        for key,value in find.items():
+            if value == 0:
+                if key.split('#')[1] in all_methods:
+                    tsv_writer.writerow([key, all_methods[key.split('#')[1]]])
+                else:
+                    print('Ctest: ' + key + ' not found')
+
+
+
+extract_ctest_cyclomatic(project_name[1])
+
